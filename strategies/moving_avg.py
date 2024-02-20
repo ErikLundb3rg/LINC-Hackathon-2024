bcd84f31864438cc5675cf5c3d24fa31502a15f4
Jonatan Kruse <jonatan.kruse@spiideo.com>
2024-02-20 20:34:34 +0100

    Add more dummy strategies

---
+++ strategies/moving_avg.py
@@ -0,0 +1,8 @@
+import time
+
+def moving_avg(capital, shouldRun):
+    print("starting moving avg strategy")
+    while shouldRun.is_set():
+        time.sleep(2)
+        print("Rebalanced for the moving avg strategy with: ", capital)
+    print("Ending moving avg strategy")
