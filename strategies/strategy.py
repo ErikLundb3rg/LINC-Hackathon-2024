3f7bef0cb1965e44f53fe61265a61afe5bc8af22
Erik Lundberg <erik.lumpa@hotmail.com>
2024-02-20 18:58:55 +0100

    add multithreading

---
+++ strategies/strategy.py
@@ -0,0 +1,23 @@
+import time
+
+class Strategy: 
+  def __init__():
+    pass 
+  
+    
+  def run(self, capital):
+    self.capital = capital
+    
+    
+    while True: 
+      time.sleep(5)
+    # sell off some stock 
+    
+   
+  
+  
+    
+  
+    
+  
+  
